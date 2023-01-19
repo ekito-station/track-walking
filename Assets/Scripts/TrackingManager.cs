7b01f16f7ea0dc97c6242071af7b8059275b95b4
ekito-line <ekito.line@gmail.com>
2023-01-20 00:26:36 +0900

    created TrackingWithSound

--- Assets/Scripts/TrackingManager.cs
+++ Assets/Scripts/TrackingManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HomeManager : MonoBehaviour
+public class TrackingManager : MonoBehaviour
 {
     Coroutine walkingRecord;
     public float waitSec;
